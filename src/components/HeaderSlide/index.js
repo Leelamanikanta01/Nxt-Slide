c1cf2c0a75719fce35d2152faa24591ca121b7bf
leelamanikanta01 <leelamanikanta01@gmail.com>
2023-04-15 09:05:21 +0000

    nxt-slide

---
+++ src/components/HeaderSlide/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const HeaderSlide = () => (
+  <div className="Header-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-slides/nxt-slides-logo.png"
+      className="nxt-slides-logo"
+      alt="nxt slides logo"
+    />
+    <h1 className="header">Nxt Slides</h1>
+  </div>
+)
+
+export default HeaderSlide
